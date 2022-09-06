919c07bc3201c95afc25a4d138006d2952617762
lorycade <lorenzo.cadente@accenture.com>
2022-09-06 17:32:28 +0200

    allineamento valutazione esperienza a comportamento cms

--- src/components/cmp-rating/cmp-rating.js
+++ src/components/cmp-rating/cmp-rating.js
@@ -1,79 +1,114 @@
-/* eslint-disable */
-const wrapper = document.querySelector('.cmp-rating');
-let activeStep = 0;
+  /* eslint-disable */
+  var wrapper = document.querySelector('.cmp-rating');
+  var activeStep = 0;
 
-function initRatings() {
-  if (!wrapper) return;
-  const radios = wrapper.querySelectorAll('.rating input');
-  const btnNext = wrapper.querySelector('.btn-next');
-  const btnBack = wrapper.querySelector('.btn-back');
-
-  radios.forEach(radio => {
-    radio.addEventListener('change', () => {
-      if(radio.checked) {
-        if (activeStep == 0) {
+  function initRatings() {
+    if (!wrapper) return;
+    var radios = wrapper.querySelectorAll('.rating input');
+    var btnNext = wrapper.querySelector('.btn-next');
+    var btnBack = wrapper.querySelector('.btn-back');
+    radios.forEach(function (radio) {
+      radio.addEventListener('change', function () {
+        /* changing the rating resets and restarts the flow */
+        resetBlock()
+        if (radio.checked) {
           openNextStep();
         }
-      }
+      });
     });
-  });
 
-  if (btnNext) {
-    btnNext.addEventListener('click', () => {
-      closePreviousStep();
-      openNextStep();
-    });
+    if (btnNext) {
+      btnNext.addEventListener('click', function () {
+        closePreviousStep();
+        openNextStep();
+      });
+    }
+
+    if (btnBack) {
+      btnBack.addEventListener('click', function () {
+        closePreviousStep();
+        backToPrevious();
+      });
+    }
   }
 
-  if (btnBack) {
-    btnBack.addEventListener('click', () => {
-      closePreviousStep();
-      backToPrevious();
-    });
+  function openNextStep() {
+    var formWrapper = wrapper.querySelector('.form-rating');
+    formWrapper.classList.remove('d-none');
+
+    var inputRadio = document.querySelector('[name="ratingA"]:checked');
+    var currentFieldset = parseInt(inputRadio.value) > 3
+      ? document.querySelector('.fieldset-rating-one')
+      : document.querySelector('.fieldset-rating-two');
+    currentFieldset.classList.remove('d-none')
+
+    var currentStepWrapper = wrapper.querySelector("[data-step=\"".concat(activeStep + 1, "\"]"));
+    currentStepWrapper.classList.remove('d-none');
+    currentStepWrapper.classList.add('active');
+
+    var steps = wrapper.querySelectorAll('[data-step]');
+    activeStep = activeStep + 1;
+
+    if (activeStep == steps.length) {
+      var cardRating = wrapper.querySelector('.cmp-rating__card-first');
+      formWrapper.classList.add('d-none');
+      cardRating.classList.add('d-none');
+      var event = new Event('feedback-submit');
+      document.dispatchEvent(event);
+    }
   }
-}
 
-function openNextStep() {
-  const steps = wrapper.querySelectorAll('[data-step]');
-  const formWrapper = wrapper.querySelector('.form-rating');
-  const cardRating = wrapper.querySelector('.cmp-rating__card-first');
-  const stepOneWrapper = wrapper.querySelector(`[data-step="${activeStep + 1}"]`);
-  formWrapper.classList.remove('d-none');
-  stepOneWrapper.classList.remove('d-none');
-  stepOneWrapper.classList.add('active');
-  activeStep = activeStep + 1;
+  function backToPrevious() {
+    // var steps = wrapper.querySelectorAll('[data-step]');
+    var formWrapper = wrapper.querySelector('.form-rating');
+    formWrapper.classList.remove('d-none');
 
-  if (activeStep == steps.length) {
+    activeStep = activeStep - 1;
+
+    if (activeStep == 0) {
+      formWrapper.classList.add('d-none');
+
+      var cardRating = wrapper.querySelector('.cmp-rating__card-first');
+      cardRating.classList.remove('d-none');
+
+      resetFieldsets();
+
+      document.querySelector('[name="ratingA"]:checked').checked = false;
+    } else {
+      var previousWrapper = wrapper.querySelector("[data-step=\"".concat(activeStep + 1, "\"]"));
+      previousWrapper.classList.add('d-none');
+      previousWrapper.classList.remove('active');
+
+      var currentWrapper = wrapper.querySelector("[data-step=\"".concat(activeStep, "\"]"));
+      currentWrapper.classList.remove('d-none');
+      currentWrapper.classList.add('active');
+    }
+  }
+
+  function resetBlock() {
+    closePreviousStep();
+
+    var formWrapper = wrapper.querySelector('.form-rating');
     formWrapper.classList.add('d-none');
-    cardRating.classList.add('d-none');
-    const event = new Event('feedback-submit');
-    document.dispatchEvent(event);
+
+    resetFieldsets();
+
+    activeStep = 0;
   }
-}
 
-function backToPrevious() {
-  const steps = wrapper.querySelectorAll('[data-step]');
-  const formWrapper = wrapper.querySelector('.form-rating');
-  const cardRating = wrapper.querySelector('.cmp-rating__card-first');
-  const previousWrapper = wrapper.querySelector(`[data-step="${activeStep - 1}"]`);
-  const currentWrapper = wrapper.querySelector(`[data-step="${activeStep}"]`);
-  formWrapper.classList.remove('d-none');
-  activeStep = activeStep - 1;
-  if (activeStep == 0) {
-    formWrapper.classList.add('d-none');
-    cardRating.classList.remove('d-none');
-  } else {
-    previousWrapper.classList.remove('d-none');
-    previousWrapper.classList.add('active');
-    currentWrapper.classList.add('d-none');
-    currentWrapper.classList.remove('active');
+  function resetFieldsets() {
+    var fieldset1 = document.querySelector('.fieldset-rating-one')
+    var fieldset2 = document.querySelector('.fieldset-rating-two')
+    fieldset1.classList.add('d-none');
+    fieldset2.classList.add('d-none');
   }
-}
 
-function closePreviousStep() {
-  const stepWrapper = wrapper.querySelector('[data-step].active');
-  stepWrapper.classList.add('d-none');
-  stepWrapper.classList.remove('active');
-}
+  function closePreviousStep() {
+    var stepWrapper = wrapper.querySelector('[data-step].active');
+    if (stepWrapper) {
+      stepWrapper.classList.add('d-none');
+      stepWrapper.classList.remove('active');
+    }
+  }
 
-initRatings();
+  initRatings();
